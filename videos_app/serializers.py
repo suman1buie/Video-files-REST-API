ba4c2917e5ca3d99341ec7ae9e2d44c694a64f83
suman mandal <48859011+suman1buie@users.noreply.github.com>
2024-11-18 12:49:26 +0530

    Update serializers.py

--- videos_app/serializers.py
+++ videos_app/serializers.py
@@ -6,18 +6,3 @@ class VideoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Video
         fields = ['id', 'video_title', 'file_url', 'video_size', 'video_duration', 'uploaded_at']
-
-
-# class SharedLinkSerializer(serializers.ModelSerializer):
-#     video_title = serializers.ReadOnlyField(source='video.title')
-#     video = serializers.SerializerMethodField()
-    
-#     class Meta:
-#         model = SharedLink
-#         fields = ['id', 'video', 'video_title', 'token', 'expiry']
-
-#     def get_video(self, obj):
-#         return {
-#             'id': obj.video.id,
-#             'video_link': obj.video.file_url
-#         }
